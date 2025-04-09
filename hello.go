4e47d468507d9608c416665f9910fe208b050f40
knetto80 <kfnetto80@gmail.com>
2025-04-08 21:12:10 -0300

    calculo

--- hello.go
+++ hello.go
@@ -2,10 +2,10 @@ package main //função principal//
 
 import "fmt" //importação de função//
 
-// K=C+273 formula temperatura de Graus Celsius em Kelvin
+// K=C+273 formula temperatura de Graus Celsius para Kelvin//
 
-const C = 100
-var K float64
+const C = 100 //temperatura em ºC //
+var K float64 // K kelvin //
 
 func main()  { //função principal//
 	K = C + 273
